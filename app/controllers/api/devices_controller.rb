6d41fd0838beca68ec2ee57affc643235643f156
Ch1keen <40013212+Ch1keen@users.noreply.github.com>
2021-11-12 01:19:12 +0900

    feat: devices#create

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -2,8 +2,18 @@
 
 module Api
   class DevicesController < ApplicationController
+    def create
+      @device = Device.create(device_params)
+      render json: {msg: true}
+    end
+
     def show
       render "show"
+    end
+
+    private
+    def device_params
+      params.require(:device).permit(:devicesn,:userid)
     end
   end
 end
