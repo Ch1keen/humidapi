9d5a12fe5fb2ddfc23ac21e358822dc3a8bc3cde
Ch1keen <40013212+Ch1keen@users.noreply.github.com>
2021-11-12 00:13:40 +0900

    feat: Devices, Logs 컨트롤러 추가

---
+++ app/controllers/api/devices_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api
+  class DevicesController < ApplicationController
+    def show
+      render "show"
+    end
+  end
+end
---
+++ app/controllers/api/logs_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api
+  class LogsController < ApplicationController
+    def show
+      render "show"
+    end
+  end
+end
